6d9a6dc025ced8ce98af00bbe3c5d13a8d8a1cfc
antonl1998 <44274534+antonl1998@users.noreply.github.com>
2020-01-30 11:35:27 +0200

    Add files via upload

---
+++ WS0/helloworld.js
@@ -0,0 +1,32 @@
+var http = require("http");
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'text/html'});
+  if(request.url === "/helloworld") {
+    response.write("<h1>Hello World</h1>");
+    response.write("<a href='/helloroutes'>Go To Hello Routes\n</a>");
+    response.write(`
+      <table>
+        <tr><td>Name</td><td>Address</td><td>City</td></tr>
+        <tr><td>Matti Meikäläinen</td><td>Timotie 1, as 10</td><td>Tampere</td></tr>
+        <tr><td>Maija Virtanen</td><td>Asematie 12</td><td>Kiljava</td></tr>
+      </table>`);
+  } else if(request.url === "/helloroutes") {
+    response.write("<h1>Hello routes</h1>");
+    response.write("<a href='/helloworld'>Go To Hello Routes\n</a>");
+  } else if(request.url === "/") {
+    response.write("<h1>TOIMII???</h1>")
+    response.write("<a href='/helloroutes'>Go To Hello Routes\n</a>");
+    response.write(`
+      <table>
+        <tr><td>Name</td><td>Address</td><td>City</td></tr>
+        <tr><td>Matti Meikäläinen</td><td>Timotie 1, as 10</td><td>Tampere</td></tr>
+        <tr><td>Maija Virtanen</td><td>Asematie 12</td><td>Kiljava</td></tr>
+      </table>`);
+  }
+
+  // Send the response body as "Hello World"
+  response.end();
+}).listen(8081);
+
+//Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
